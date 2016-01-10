7da292379bf7e2ffaaa2436c496add47adf2fe62
楼教主 <cik520@qq.com>
2016-01-10 16:14:32 +0800

    添加代码风格检查

--- index.js
+++ index.js
@@ -1,62 +1,80 @@
+/**
+ * @file classnames
+ * @author 52cik
+ * @email fe.52cik@gmail.com
+ */
+
 (function (prototype) {
-  var toString = prototype.toString;
-  var hasOwn = prototype.hasOwnProperty;
+    'use strict';
 
-  var utils = {
-    parse: function (resultSet, arg) {
-      if (!arg) return;
+    var toString = prototype.toString;
+    var hasOwn = prototype.hasOwnProperty;
 
-      var type = toString.call(arg);
-      utils[type] && utils[type](resultSet, arg);
-    },
-    '[object Array]': function (resultSet, arg) {
-      for (var i = 0, len = arg.length; i < len; i++) {
-        utils.parse(resultSet, arg[i]);
-      }
-    },
-    '[object Object]': function (resultSet, arg) {
-      for (var k in arg) {
-        if ( hasOwn.call(arg, k) ) {
-          if ( arg[k] ) {
-            resultSet[k] = true;
-          } else {
-            delete resultSet[k];
-          }
+    var utils = {
+        'parse': function (resultSet, arg) {
+            var type;
+
+            if (!arg) { return; }
+
+            type = toString.call(arg);
+            utils[type] && utils[type](resultSet, arg);
+        },
+        '[object Array]': function (resultSet, arg) {
+            var i = 0;
+            var len = arg.length;
+
+            for (; i < len; i++) {
+                utils.parse(resultSet, arg[i]);
+            }
+        },
+        '[object Object]': function (resultSet, arg) {
+            var k;
+
+            for (k in arg) {
+                if (hasOwn.call(arg, k)) {
+                    if (arg[k]) {
+                        resultSet[k] = true;
+                    } else {
+                        delete resultSet[k];
+                    }
+                }
+            }
+        },
+        '[object String]': function (resultSet, arg) {
+            var i = 0;
+            var arr = arg.split(/\s+/);
+            var len = arr.length;
+
+            for (; i < len; i++) {
+                resultSet[arr[i]] = true;
+            }
+        },
+        '[object Number]': function (resultSet, arg) {
+            resultSet[arg] = true;
         }
-      }
-    },
-    '[object String]': function (resultSet, arg) {
-      var arr = arg.split(/\s+/);
+    };
 
-      for (var i = 0, len = arr.length; i < len; i++) {
-        resultSet[arr[i]] = true;
-      }
-    },
-    '[object Number]': function (resultSet, arg) {
-      resultSet[arg] = true;
-    }
-  };
+    function classNames() {
+        var k;
+        var list = [];
+        var classSet = {};
 
-  function classNames () {
-    var list = [];
-    var classSet = {};
+        utils['[object Array]'](classSet, arguments);
 
-    utils['[object Array]'](classSet, arguments);
+        for (k in classSet) {
+            if (hasOwn.call(classSet, k) && classSet[k]) {
+                list.push(k);
+            }
+        }
 
-    for (var k in classSet) {
-      if (hasOwn.call(classSet, k) && classSet[k]) {
-        list.push(k);
-      }
+        return list.join(' ');
     }
 
-    return list.join(' ');
-  };
-
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = classNames;
-  } else if (typeof define === 'function' && define.amd ) {
-    define('classnames', [], function () { return classNames });
-  } else {
-    window.classNames = classNames;
-  }
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = classNames;
+    } else if (typeof define === 'function') {
+        define('classnames', [], function () { return classNames; });
+    } else {
+        window.classNames = classNames;
+    }
 })(Object.prototype);
